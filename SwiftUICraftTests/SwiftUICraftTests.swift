3cc3be5c0e0eb512b0366a6c5f02a8e88dbf8369
Michael Fluharty <michael@fluharty.com>
2026-05-02 19:05:57 -0500

    Initial Commit

---
+++ SwiftUICraftTests/SwiftUICraftTests.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUICraftTests.swift
+//  SwiftUICraftTests
+//
+//  Created by Michael Fluharty on 5/2/26.
+//
+
+import Testing
+@testable import SwiftUICraft
+
+struct SwiftUICraftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
